61ad6a0e635d17dfd909e6f4e1427ae9137abb89
Hrabal <federicocerchiari@gmail.com>
2017-02-01 22:28:17 +0100

    gnappate random tutto lignorecase

--- minestra_bot.py
+++ minestra_bot.py
@@ -1,8 +1,17 @@
 import telebot
 import re
+import random
 
 bot = telebot.TeleBot(token='277842064:AAFflOSXBvno7qgkHKz9aga09R2xSKJTpDM')
 
+gnappate = [
+    'Io e tizie scopate',
+    'Mai una gioia',
+    'Scrivi a stocazzo@staminchia.gnappobesutti',
+    'pew pew pew, che entusiasmo',
+    'Non capire is the new non capire',
+    'Aaaaaawww!',
+]
 
 def minestra():
     return open('minestra.png', 'rb')
@@ -19,14 +28,19 @@ def ho_fame(message):
     bot.send_photo(message.chat.id, minestra())
 
 
-@bot.message_handler(regexp='buono')
+@bot.message_handler(regexp=re.compile(r'buono', re.I))
 def buono(message):
     bot.send_message(message.chat.id, 'Le cose più buone sono quelle diy, con lo zenzero e la carota viola.')
     bot.send_photo(message.chat.id, minestra())
 
 
-@bot.message_handler(regexp='fallito')
+@bot.message_handler(regexp=re.compile(r'fallito', re.I))
 def fallito(message):
     bot.send_message(message.chat.id, 'Ma no, non dire così! Fatti una minestra di Gnappo!')
+
+
+@bot.message_handler(regexp=re.compile(r'gnappo', re.I))
+def fallito(message):
+    bot.send_message(message.chat.id, random.choice(gnappate))
 
 bot.polling()
